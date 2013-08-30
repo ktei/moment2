bf37218ae774d605b4a6d7e71062baef6a9d814d
Rui Cheng <ktei2008@gmail.com>
2013-08-30 20:49:01 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moment2::Application.config.secret_key_base = '7ff993752a186b99fd6930bac2d5d3e2b4245d2ed9ff363a2c94c7628fe9be54a41a2a20f0c1a269faa8f3b877204296b566f7c6c5a30e1016b8db43776de493'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Moment2::Application.config.session_store :cookie_store, key: '_moment2_session'
